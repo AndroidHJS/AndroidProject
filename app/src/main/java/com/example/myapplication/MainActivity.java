37a4e979f6e1fb8271907968304501434ea2be9e
AndoridHJS <18665597926@163.com>
2016-12-03 20:02:43 +0800

    修改了写文件

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTvText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTvText= (TextView) findViewById(R.id.tv_test);
+        mTvText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTvText.setText("我点击了下");
+                Toast.makeText(MainActivity.this,"点击了",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
