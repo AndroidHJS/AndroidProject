084be4c411cc50fa5f04dcb5aea0c97c2ef519e9
AndoridHJS <18665597926@163.com>
2016-12-07 19:42:43 +0800

    完成模块干货的编写 ，但是一些细节问题还未处理

---
+++ app/src/main/java/com/jack/adapter/GanHuoAdapter.java
@@ -0,0 +1,76 @@
+package com.jack.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.jack.bean.GanHuoBean;
+import com.jack.bean.GanIO;
+import com.jack.main.R;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Administrator on 2016/12/7.
+ */
+
+public class GanHuoAdapter  extends RecyclerView.Adapter<GanHuoAdapter.GanHuoViewHolder> {
+    private Context mContext;
+    private List<GanHuoBean> mData;
+    public  GanHuoAdapter(Context Context, List<GanHuoBean> mData){
+        this.mContext=Context;
+        this.mData=mData;
+    }
+
+    @Override
+    public GanHuoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View  view=View.inflate(mContext, R.layout.item_ganhuo,null);
+        GanHuoViewHolder holder=new GanHuoViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(GanHuoViewHolder holder, int position) {
+        GanHuoBean ganHuoBean = mData.get(position);
+        holder.mTvTitle.setText(ganHuoBean.getDesc());
+        if(ganHuoBean.getImages()!=null){
+            Glide.with(mContext).load(ganHuoBean.getImages()[0]).into(holder.mImgProject);
+        }
+        holder.mTvTime.setText(ganHuoBean.getCreatedAt());
+        holder.mTvName.setText(ganHuoBean.getWho());
+
+
+    }
+   public  void addData( List<GanHuoBean> mData){
+       mData.addAll(mData);
+       this.notifyDataSetChanged();
+
+   }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+   static class GanHuoViewHolder extends RecyclerView.ViewHolder{
+         @Bind(R.id.tv_project_decs)
+         TextView mTvTitle;
+        @Bind(R.id.img_project_img)
+         ImageView mImgProject;
+        @Bind(R.id.tv_project_who)
+         TextView mTvName;
+        @Bind(R.id.tv_project_time)
+         TextView mTvTime;
+        public GanHuoViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+}
--- app/src/main/java/com/jack/fragment/GanioFragment.java
+++ app/src/main/java/com/jack/fragment/GanioFragment.java
@@ -1,24 +1,35 @@
 package com.jack.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
 import com.jack.Net.RequestListener;
 import com.jack.Net.RetrofitHelper;
+import com.jack.adapter.GanHuoAdapter;
+import com.jack.bean.GanHuoBean;
 import com.jack.bean.GanIO;
 import com.jack.comment.base.BaseFragment;
 import com.jack.main.R;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
-import butterknife.OnClick;
+import java.util.List;
+
+import butterknife.Bind;
+
 
 /**
  * Created by Administrator on 2016/12/4.
  */
 
 public class GanioFragment extends BaseFragment {
-
-
+    @Bind(R.id.recyclerview_ganHuo)
+    XRecyclerView mXRecyclerView;
+    private static  int PAGE_NUM = 1;
+    private static final int PAGE = 20;
+    private GanHuoAdapter mAdapter;
+    private  boolean mIsLoadRefresh=true;
     @Override
     public View getView(LayoutInflater inflater) {
         View view=inflater.inflate(R.layout.fragment_ganio,null);
@@ -27,12 +38,52 @@ public class GanioFragment extends BaseFragment {
 
     @Override
     public void initData() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(mActiviy);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mXRecyclerView.setLayoutManager(layoutManager);
+        mXRecyclerView.setLoadingListener(new XRecyclerView.LoadingListener() {
+            @Override
+            public void onRefresh() {
+                mIsLoadRefresh=false;
+                PAGE_NUM=1;
+                requestData(PAGE_NUM);
+            }
+
+            @Override
+            public void onLoadMore() {
+                PAGE_NUM++;
+                requestData(PAGE_NUM);
+            }
+        });
+        requestData(PAGE_NUM);
+    }
+    private  void bindGanHuoData(List<GanHuoBean> mData){
+        if(!mIsLoadRefresh){
+            mAdapter=new GanHuoAdapter(mActiviy,mData);
+            mXRecyclerView.setAdapter(mAdapter);
+            mIsLoadRefresh=true;
+            mXRecyclerView.refreshComplete();
+            return;
+        }
+        if(mAdapter==null){
+            mAdapter=new GanHuoAdapter(mActiviy,mData);
+            mXRecyclerView.setAdapter(mAdapter);
+            mAdapter.addData(mData);
+            mXRecyclerView.loadMoreComplete();
+        }
+
+
+
+
+    }
+    private void requestData(int page){
         RetrofitHelper.getGanHuoInfo(new RequestListener() {
             @Override
             public void onSuccess(Object obj) {
                 if(obj instanceof GanIO){
                     GanIO ganHuo= (GanIO) obj;
-                    Toast.makeText(mActiviy,ganHuo.getResults().get(0).getDesc(),Toast.LENGTH_LONG).show();
+                    bindGanHuoData(ganHuo.getResults());
+
                 }
 
             }
@@ -41,8 +92,7 @@ public class GanioFragment extends BaseFragment {
             public void onFail(Throwable throwable) {
 
             }
-        },20,1, GanIO.class);
+        },PAGE,page, GanIO.class);
 
     }
-
 }
